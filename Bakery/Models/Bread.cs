dd6e3b376ec6d1058713902667fa0a9236a78d8f
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-06-30 22:19:50 -0400

    pass test for set TotalLoaves

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -7,7 +7,7 @@ namespace Bread.Models {
 
   public class BreadOrder
   {
-    public int TotalLoaves { get; }
+    public int TotalLoaves { get; set; }
 
     public BreadOrder(int totalLoaves)
     {
