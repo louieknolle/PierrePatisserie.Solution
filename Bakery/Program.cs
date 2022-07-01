487e501ad8bb3ca113ce45994f09a1b86088563f
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-06-30 21:50:41 -0400

    add static Main() to program

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -3,3 +3,14 @@ using System.Collections.Generic;
 using Bakery.Models;
 using Bread.Models;
 using Pastry.Models;
+
+namespace Bakery
+{
+  public class Program
+  {
+   public static void Main() 
+   {
+    
+   }
+  }
+}
