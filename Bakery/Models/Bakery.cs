6914999f55ec451ca463e8c311e5c4c9ba0df0a5
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-07-01 22:19:59 -0400

    write program.cs

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -3,10 +3,16 @@ using System;
 using Bread.Models;
 using Pastry.Models;
 
-namespace Bakery.Models {
+namespace Bakery.Models 
+{
+  // // public class Bakery
+  // {
 
-  public static class Bakery
-  {
-    
-  }
+
+  //   public int TotalBakeryOrder()
+  //   {
+  //     int orderTotal = newBreadOrder.FindBreadOrderTotal() + newPastryOrder.FindPastryOrderTotal();
+  //     return orderTotal;
+  //   }
+  // }
 }
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,7 +10,40 @@ namespace Bakery
   {
    public static void Main() 
    {
-    
+    Console.WriteLine("Welcome to Pierre's Patisserie! Home of fine bread and fine pastries. \n Pricing for bread:Buy 2 loaves, get one free! Single loaf = $5, two loaves = $10 and three loaves = $10! \n Pricing for pastry: 1 or 2 pastries = $2 each, 3 pastries = $5, 4 pastries = $7, 5 pastries = $9, 6 pastries = $10!");
+
+    Console.WriteLine("How many loaves of bread would you like to order? Since we're a small business, current limit is three loaves.");
+    BreadOrder newBreadOrder = new BreadOrder(Int32.Parse(Console.ReadLine()));
+
+    if (newBreadOrder.TotalLoaves > 3 || newBreadOrder.TotalLoaves < 0 )
+    {
+      Console.WriteLine("Please enter 1, 2, or 3 loaves to buy.");
+
+      Main();
+    }
+    else 
+    {
+      int breadTotal = newBreadOrder.FindBreadOrderTotal();
+      Console.WriteLine("Your bread order comes to $" + breadTotal + ".");
+    }
+
+    Console.WriteLine("How many pastries would you like to order? Current max pastry order is 6 pastries, thanks for understanding!");
+    PastryOrder newPastryOrder = new PastryOrder(Int32.Parse(Console.ReadLine()));
+
+    if (newPastryOrder.TotalPastry > 6 || newPastryOrder.TotalPastry < 0 )
+    {
+      Console.WriteLine("Please enter between 1 and 6 pastries to buy.");
+
+      Main();
+    }
+    else 
+    {
+      int pastryTotal = newPastryOrder.FindPastryOrderTotal();
+      Console.WriteLine("Your pastry order comes to $" + pastryTotal + ".");
+    }
+
+    int orderTotal = newBreadOrder.FindBreadOrderTotal() + newPastryOrder.FindPastryOrderTotal(); 
+    Console.WriteLine("Your order comes to $" + orderTotal + ".Thanks for supporting us and enjoy your treats!");
    }
   }
 }
